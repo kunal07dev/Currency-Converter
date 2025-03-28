c2d6faee8f9e74d8442b488fffd6e05cbf02e422
Kunal <kunalgpt27@gmail.com>
2025-03-28 23:55:38 +0530

    commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.currencyconverter"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.currencyconverter"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation ("com.google.android.material:material:1.12.0") // Use the latest version available
+    implementation ("androidx.core:core-ktx:1.13.1") // Add this to ensure compatibilit
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
